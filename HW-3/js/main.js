bd90d723957c9fbd527e7d5db6f26c0bae780453
Mr2nec <vladislav.shpilka@gmail.com>
2020-10-28 17:25:59 +0200

    commit

--- HW-3/js/main.js
+++ HW-3/js/main.js
@@ -1,25 +1,33 @@
-async function jsonPost(url, data) {
-    let response = await fetch(url, {
-        method: 'POST',
-        body: JSON.stringify(data),
-    });
+function jsonPost(url, data) {
+    return new Promise((resolve, reject) => {
+        var x = new XMLHttpRequest();
+        x.onerror = () => reject(new Error('jsonPost failed'));
+        //x.setRequestHeader('Content-Type', 'application/json');
+        x.open('POST', url, true);
+        x.send(JSON.stringify(data));
 
-    if (response.status == 200) {
-        let json = await response.json();
-        return json;
-    }
+        x.onreadystatechange = () => {
+            if (x.readyState == XMLHttpRequest.DONE && x.status == 200) {
+                resolve(JSON.parse(x.responseText));
+            } else if (x.status != 200) {
+                reject(new Error('status is not 200'));
+            }
+        };
+    });
 }
 
 function* async() {
-    let { data, nexMessageId } = yield jsonPost(
+    let { data, nextMessageId } = yield jsonPost(
         'http://students.a-level.com.ua:10012',
         {
             func: 'getMessages',
         }
     );
     console.log(data);
+    console.log(nextMessageId);
 }
 
 let iter = async();
-let tmp1 = iter.next();
-tmp1.value.then((res) => iter.next(res));
+iter.next()
+    .value.then((res) => iter.next(res))
+    .catch((e) => console.log(e));
