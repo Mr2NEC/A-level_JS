5db106ce894a5e28ab4b7dc44fdf7f865569f4f2
Mr2nec <vladislav.shpilka@gmail.com>
2020-11-10 14:15:04 +0200

    commit

---
+++ HW-6/js/redux.js
@@ -0,0 +1,131 @@
+function createStore(reducer) {
+    let state = reducer(undefined, {});
+    let cbs = [];
+
+    function dispatch(action) {
+        if (typeof action === 'function') {
+            return action(dispatch);
+        }
+        let newState = reducer(state, action);
+        if (state !== newState) {
+            state = newState;
+            for (let cb of cbs) cb();
+        }
+    }
+
+    return {
+        getState() {
+            return state;
+        },
+        dispatch: dispatch,
+        subscribe(cb) {
+            cbs.push(cb);
+            return () => {
+                cbs = cbs.filter((someElement) => someElement !== cb);
+            };
+        },
+    };
+}
+
+const store = createStore(
+    (state = {}, { name, type, status, payload, error }) =>
+        type === 'PROMISE'
+            ? (state = { ...state, [name]: { status, payload, error } })
+            : state
+);
+
+store.subscribe(() => console.log(store.getState()));
+
+const actionPromise = (name, p) => {
+    const actionPending = () => ({ name, type: 'PROMISE', status: 'PENDING' });
+    const actionResolved = (payload) => ({
+        name,
+        type: 'PROMISE',
+        status: 'RESOLVED',
+        payload,
+    });
+    const actionRejected = (error) => ({
+        name,
+        type: 'PROMISE',
+        status: 'REJECTED',
+        error,
+    });
+    return async (dispatch) => {
+        try {
+            dispatch(actionPending());
+            let result = await p;
+            dispatch(actionResolved(result));
+            return result;
+        } catch (e) {
+            dispatch(actionRejected(e));
+        }
+    };
+};
+
+const gql = (
+    url = 'http://shop-roles.asmer.fs.a-level.com.ua/graphql',
+    query = '',
+    variables = {}
+) =>
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ query, variables }),
+    }).then((res) => res.json());
+
+store.dispatch(
+    actionPromise(
+        'CategoryFind',
+        gql(
+            undefined,
+            `
+query cats($q: String){
+    CategoryFind(query:$q){
+        _id name goods{
+            _id name
+        }
+    }
+}
+`,
+            { q: JSON.stringify([{}]) }
+        ).then((data) => data.data)
+    )
+);
+
+// const actionDefferredPromise = (ms, getPromise) => async (dispatch) => {
+//     await dispatch(actionPromise('delay', delay(ms))); //REGISTER
+//     await dispatch(actionPromise('defferred', getPromise())); //LOGIN
+// };
+
+const actionLogin = (login, password) =>
+    actionPromise('login', loginQuery(login, password));
+
+const actionRegister = (login, password) => async (dispatch) => {
+    await dispatch('register', actionPromise(registerQuery(login, password))); //REGISTER
+    await dispatch(actionLogin(login, password)); //LOGIN
+};
+
+let ARBody = document.querySelector('.authorizationRegistrationBody');
+
+for (let node of ARBody.childNodes) {
+    node.addEventListener('click', function (event) {
+        changeLoginOrRegister(event.target.id);
+    });
+}
+
+function changeLoginOrRegister(id) {
+    let id2 = id === 'authorization' ? 'registration' : 'authorization';
+    let elem = document.querySelector(`#${id}`);
+    let elem2 = document.querySelector(`#${id2}`);
+    if (elem.style.backgroundColor !== 'green') {
+        elem.style.backgroundColor = 'green';
+        elem.style.color = 'honeydew';
+        elem.style.border = '1px solid honeydew';
+        elem2.style.backgroundColor = 'white';
+        elem2.style.color = 'black';
+        elem2.style.border = '1px solid black';
+    }
+}
